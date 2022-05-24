f9107e7d2330b3b79d238da06d155a9de5fcbde0
Richard Aleixo <richard.aleiixo@gmail.com>
2022-05-24 17:54:38 +0200

    fin seccion 3 - diseño

---
+++ src/app/pages/nopage/nopage.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nopage',
+  templateUrl: './nopage.component.html',
+  styleUrls: [ './nopage.component.css']
+})
+export class NopageComponent  {
+
+  year = new Date().getFullYear();
+}
